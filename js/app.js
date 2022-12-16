ec93324244c9e7f30dae4d5ff04feeb11e200230
nhattruong1234 <nguyenphamnhattruong2001@gmail.com>
2022-12-16 09:07:14 +0700

    update app.js

---
+++ js/app.js
@@ -0,0 +1,84 @@
+var customer = [
+    {
+        username: 'vanthien',
+        password: 'vanthien',
+        info: {
+            name: "Vân Thiên",
+            sex: "male",
+            age: 18,
+            address: "đoán xem"
+        }
+    },
+    {
+        username: 'nhaty',
+        password: 'nhaty',
+        info: {
+            name: "Nhất Ý",
+            sex: "male",
+            age: 25,
+            address: "hỏi làm gì"
+        }
+    },
+    {
+        username: 'hainhu',
+        password: 'hainhu',
+        info: {
+            name: "Hải Như",
+            sex: "female",
+            age: 15,
+            address: "không cho biết đâu"
+        }
+    },
+]
+
+var action = {
+    login: 0,
+    userlogin: ""
+}
+
+var button = document.getElementById('btn_login');
+button.onclick = function () {
+    var username = document.getElementById("user").value;
+    var password = document.getElementById("password").value;
+    let tmp = 0;
+    for (var i = 0; i < customer.length; i++) {
+        if (username == customer[i].username && password == customer[i].password) {
+            tmp = 0;
+            window.location = "../html/service.html"
+        }
+        else {
+            tmp++;
+        }
+    }
+    if (tmp != 0) {
+        document.getElementById("error").innerHTML = "Username or password is incorrect";
+    }
+       
+};
+
+// function myFunction(login) {
+//     if(login != 0){
+//         var ava = document.getElementById("ava").innerHTML ="hello"
+//         return ava;
+//     }
+// }
+// window.myFunction(action.login); 
+
+// Product-details page of Amounnt
+function increment() {
+    document.getElementById('demoInput').stepUp();
+ }
+ function decrement() {
+    document.getElementById('demoInput').stepDown();
+}
+
+// Product-details page of Comment
+var post= document.getElementById("send");
+post.addEventListener("click", function(){
+    var commentBoxValue= document.getElementById("comment-box").value;
+    var p = document.createElement("p");
+    p.innerHTML="<b>Me: </b>";
+    var text = document.createTextNode(commentBoxValue);
+    p.appendChild(text);
+    document.getElementById("customercomment1").appendChild(p);
+});
