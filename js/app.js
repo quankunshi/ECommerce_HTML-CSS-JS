cbcdffa921ff133e95a9194d45b66d1e61efec45
nhattruong1234 <91730146+nhattruong1234@users.noreply.github.com>
2022-12-16 09:26:08 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -63,3 +63,22 @@ button.onclick = function () {
 //     }
 // }
 // window.myFunction(action.login); 
+
+// Product-details page of Amounnt
+function increment() {
+    document.getElementById('demoInput').stepUp();
+ }
+ function decrement() {
+    document.getElementById('demoInput').stepDown();
+}
+
+// Product-details page of Comment
+var post= document.getElementById("send");
+post.addEventListener("click", function(){
+    var commentBoxValue= document.getElementById("comment-box").value;
+    var p = document.createElement("p");
+    p.innerHTML="<b>Me: </b>";
+    var text = document.createTextNode(commentBoxValue);
+    p.appendChild(text);
+    document.getElementById("customercomment1").appendChild(p);
+});
