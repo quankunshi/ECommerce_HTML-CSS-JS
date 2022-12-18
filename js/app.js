28f57edb7eb81cf1deddaa51a610309cb5732cc5
Nguyễn Minh Quân <99945660+quanmathip@users.noreply.github.com>
2022-12-18 14:35:32 +0700

    up

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ var customer = [
         password: 'vanthien',
         info: {
             name: "Vân Thiên",
+            email: "email@gmail.com",
             sex: "male",
             age: 18,
             address: "đoán xem",
@@ -16,6 +17,7 @@ var customer = [
         password: 'nhaty',
         info: {
             name: "Nhất Ý",
+            email: "email@gmail.com",
             sex: "male",
             age: 25,
             address: "hỏi làm gì",
@@ -27,6 +29,7 @@ var customer = [
         password: 'hainhu',
         info: {
             name: "Hải Như",
+            email: "email@gmail.com",
             sex: "female",
             age: 15,
             address: "không cho biết đâu",
@@ -34,8 +37,13 @@ var customer = [
         }
     },
 ]
+
 // login page
 if (window.location.href == "http://127.0.0.1:5500/html/login.html") {
+    localStorage.setItem('action', JSON.stringify({
+        login: false,
+        userlogin: ``
+    }));
     var button = document.getElementById('btn_login');
 
     button.onclick = function () {
@@ -66,57 +74,99 @@ if (window.location.href == "http://127.0.0.1:5500/html/login.html") {
 
     };
 }
+// // register
+// if (window.location.href == "http://127.0.0.1:5500/html/register.html") {
+//     localStorage.setItem('account', JSON.stringify({
+//         "acc": [
+//             {
+//                 username: '',
+//                 password: '',
+//                 info: {
+//                     name: "",
+//                     email: "",
+//                     sex: "",
+//                     age: "",
+//                     address: "",
+//                     avatarurl: ""
+//                 }
 
+//             },
+//         ]
+//     }));
+//     var buttonxx = document.getElementById('btn_register');
+//     buttonxx.onclick = function () {
+//         var tmp = JSON.parse(localStorage.getItem('account'))
+//         const { username, password, info } = tmp.acc[0]
+//         username = document.getElementById("username").value
+//         password = document.getElementById("password").value
+//         info.name = document.getElementById("firstname").value + document.getElementById("lasttname").value
+//         info.email = document.getElementById("userEmail").value
+//         console.log(tmp.acc[0]);
+//     }
+// }
 // avatar change when login
 var tmp = JSON.parse(localStorage.getItem('action'))
 console.log(tmp);
 const { login, userlogin } = tmp;
 
-var avatar = document.getElementById("log").innerHTML =
-    `
+if (login == true) {
+    var avatar = document.getElementById("log").innerHTML =
+        `
 <div class="avatar">
     <!-- Avatar image -->
-    <button id="btn_avatar"><img class="avatar__image" src="/images/avatar/${userlogin}avatar.jpg"/></button>
+    <p  id ="btn_avatar"><img class="avatar__image" src="/images/avatar/${userlogin}avatar.jpg"/><p>
 </div>
 `
-
-// menu login/loguot
-var menu_check = document.getElementById('btn_avatar');
-menu_check.onclick = function () {
-    console.log(1);
-    document.getElementById("menu_icon").innerHTML =
-        `
-        <div id="menu_icon">
-    <a href="http://">settings and security</a> <br>
-    <a href="http://">login</a> <br>
-    <a href="http://">logout</a>
-    </div>
-    `
-
 }
 
-// function myFunction(login) {
-//     if(login != 0){
-//         var ava = document.getElementById("ava").innerHTML ="hello"
-//         return ava;
-//     }
-// }
-// window.myFunction(action.login); 
+
+// menu login/loguot
+var menu_check = document.getElementById("btn_avatar")
+menu_check.onclick = function () {
+    if (login == true) {
+        document.getElementById("menu_x").innerHTML =
+            `
+        <div id="menu_icon_1">
+        <a href="#">profile</a> <br>
+        <a href="#">help</a> <br>
+        <a href="#">settings and security</a>
+        <a href="login.html">logout</a> <br>
+        </div>
+        `
+    }
+    else {
+        document.getElementById("menu_x").innerHTML =
+            `
+        <div id="menu_icon_2">
+        <a href="login.html">login</a> <br> 
+        </div>
+        `
+    }
+
+
+}
+// when mouse leave menu =>> menu off
+var mouseleave = document.getElementById("menu_x")
+mouseleave.onmouseleave = function () {
+    document.getElementById("menu_x").innerHTML =
+        `
+    `
+}
 
 // Product-details page of Amounnt
 function increment() {
     document.getElementById('demoInput').stepUp();
- }
- function decrement() {
+}
+function decrement() {
     document.getElementById('demoInput').stepDown();
 }
 
 // Product-details page of Comment
-var post= document.getElementById("send");
-post.addEventListener("click", function(){
-    var commentBoxValue= document.getElementById("comment-box").value;
+var post = document.getElementById("send");
+post.addEventListener("click", function () {
+    var commentBoxValue = document.getElementById("comment-box").value;
     var p = document.createElement("p");
-    p.innerHTML="<b>Me: </b>";
+    p.innerHTML = "<b>Me: </b>";
     var text = document.createTextNode(commentBoxValue);
     p.appendChild(text);
     document.getElementById("customercomment1").appendChild(p);
